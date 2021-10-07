cc3ff596cdfa354b3c97a33bb24e1300dac4b747
RaunakBag <raunakbag97@gmail.com>
2021-10-07 20:16:25 +0530

    Array Methods Completed

---
+++ DS-Algo/Arrays/ReverseArray.js
@@ -0,0 +1,30 @@
+// const arr1 = [1, 2, 3, 4, 5];
+// let arr2 = [];
+
+
+// CONVERT  ARRAY TO STRING
+// for(let i = arr1.length-1; i>=0; i--){
+//     arr2 += arr1[i]
+// }
+// console.log(arr2);
+
+// PUSH METHOD
+// for (let i = arr1.length - 1; i >= 0; i--) {
+//     arr2.push(arr1[i])
+// }
+// console.log(arr2)
+
+// REVERSE METHOD
+// arr1.reverse();
+// console.log(arr1)
+
+// REVERSE METHOD WITH A NEW ARRAY
+// const arr2 = arr1.concat().reverse();
+// console.log(arr2);
+
+// SPREAD OPERATOR
+// const arr2 = [...arr1].reverse();
+// console.log(arr2);
+
+
+
---
+++ DS-Algo/Arrays/arrayMethods.js
@@ -0,0 +1,24 @@
+const arr1 = [1, 2, 3, 4, 5]
+const arr2 = ["A", "B", "C", "D", "E"]
+const arr3 = ["a", "b", "c", "d", "e"]
+
+// This will print the array
+// arr1.forEach((item, index, array) =>{
+//     console.log('a[' + index + ']='  +item)
+// })
+// This will give the sum of the elements of the array
+// let sum = 0
+// arr1.forEach(item =>{
+//     sum += item;
+// })
+// console.log(sum)
+// This will show how many times an element is present in an array
+// let count = {}
+// arr2.forEach(item =>{
+//     if(count[item]){
+//         count[item] ++
+//     }else{
+//         count[item] = 1;
+//     }
+// })
+// console.log(count)
